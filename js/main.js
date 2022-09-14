4a88922ef9a2fec85b0cdfdd1b0910c37176b3ba
TFuray <pk.master881@gmail.com>
2022-09-14 09:27:26 -0700

    fixed start of game

--- js/main.js
+++ js/main.js
@@ -1,21 +1,53 @@
 
-// button to play next hand
-// document.querySelector('button').addEventListener('click', firstDeal)
+let deckId = ``
+let player1Main = []
+let player2Main = []
+document.querySelector('#start-game').addEventListener('click',firstDealPlayer1)
+document.querySelector('#start-game').addEventListener('click', firstDealPlayer2)
 
-function firstDeal() {
-    const url = `https://deckofcardsapi.com/api/deck/new/draw/?count=26`
+fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        deckId = data.deck_id
+    }) 
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
 
+function firstDealPlayer1() {
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=26`
     fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            let deckId = data.deck_id
-            let player1Main = {}
-            
+            data.cards.forEach(element => {
+                player1Main.push(element.code)
+            })
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
+    return player1Main
 }
 
-firstDeal()
+
+function firstDealPlayer2(){
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=26`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            data.cards.forEach(element => {
+                player2Main.push(element.code)
+            })
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+    return player2Main
+}
+
+
+console.log(player1Main);
+console.log(player2Main);
