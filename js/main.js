b273f9171d20d29ec3d2b60a48f0d1b24224e5f1
TFuray <pk.master881@gmail.com>
2022-09-14 08:10:04 -0700

    dealing first hand

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
-// The Game of War -- @TFuray
 
-//set variable for deck id
-let deckId = ''
-// add event listener to play button
-document.querySelector('button').addEventListener('click', dealNext)
+// button to play next hand
+// document.querySelector('button').addEventListener('click', firstDeal)
 
-// initial assemble of pre shuffled deck
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        deckId = data.deck_id   
-    })
-    .catch(err =>{
-        console.log(`error ${err}`);
-    })
+function firstDeal() {
+    const url = `https://deckofcardsapi.com/api/deck/new/draw/?count=26`
 
-
-// dealing cards for each player
-fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=26/pile/player1Main/add`)
-
-
-
-
-function dealNext(){
-    return 1
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let deckId = data.deck_id
+            let player1Main = {}
+            
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 }
+
+firstDeal()
