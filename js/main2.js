94db79c68582cd6b58fc1b4815fb2aeb9c5215f6
TFuray <pk.master881@gmail.com>
2022-09-15 15:04:28 -0700

    added functionality

--- js/main2.js
+++ js/main2.js
@@ -3,10 +3,12 @@ War the Card Game -- TFuray
 */
 
 let deckId = ''
-
+let player1Val
+let compVal
 // button to play next hand
 const playCards = document.querySelector('#deal')
 const startGame = document.querySelector('#start-game')
+const results = document.querySelector('h3')
 
 playCards.addEventListener('click', playCardPlayer)
 startGame.addEventListener('click', fetchGame)
@@ -15,7 +17,7 @@ startGame.addEventListener('click', fetchGame)
 fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
         deckId = data.deck_id // set deckId global variable
     })
     .catch(err => {
@@ -28,20 +30,11 @@ function playCardPlayer() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            // console.log(data)
             document.querySelector('#player1').src = data.cards[0].image
-            playCardComp()
-            // document.querySelector('#player2').src = data.cards[1].image
-            // let player1Val = convertToNum(data.cards[0].value)
-            // let player2Val = convertToNum(data.cards[1].value)
-            // if (player1Val > player2Val) {
-            //     document.querySelector('h3').innerText = 'player 1 wins'
-            // } else if (player1Val < player2Val) {
-            //     document.querySelector('h3').innerText = 'player 2 wins'
-            // } else {
-            //     document.querySelector('h3').innerText = 'Time for war'
-            // }
-
+            player1Val = convertToNum(data.cards[0].value)
+            let cardPlayed = data.cards[0].code.toString()
+            playCardComp(player1Val, cardPlayed)
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -49,27 +42,21 @@ function playCardPlayer() {
 }
 
 
-function playCardComp(){
+function playCardComp(player1, player1Card) {
     const url = `https://deckofcardsapi.com/api/deck/${deckId}/pile/comp/draw/?count=1`
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            // console.log(data)
             document.querySelector('#player2').src = data.cards[0].image
-            // document.querySelector('#player2').src = data.cards[1].image
-            // let player1Val = convertToNum(data.cards[0].value)
-            // let player2Val = convertToNum(data.cards[1].value)
-            // if (player1Val > player2Val) {
-            //     document.querySelector('h3').innerText = 'player 1 wins'
-            // } else if (player1Val < player2Val) {
-            //     document.querySelector('h3').innerText = 'player 2 wins'
-            // } else {
-            //     document.querySelector('h3').innerText = 'Time for war'
-            // }
+            compVal = convertToNum(data.cards[0].value)
+            let cardPlayed = data.cards[0].code.toString()
+            checkWinninghand(player1, compVal, player1Card, cardPlayed)
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
+
 }
 
 
@@ -86,33 +73,126 @@ function dealPiles(cards, pile) {
 }
 
 
-function fetchGame(){
+function fetchGame() {
     const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            // console.log(data);
             dealGame(data)
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
-        
+
 }
 
 
-function dealGame(data){
+function dealGame(data) {
     let playerCards = []
     let compCards = []
-    
+
     for (let i = 0; i <= 25; i++) {
         playerCards.push(data.cards[i].code)
     }
     for (let i = 26; i <= 51; i++) {
         compCards.push(data.cards[i].code)
     }
-
     dealPiles(playerCards.toString(), 'player'); dealPiles(compCards.toString(), 'comp')
+}
+
+
+function checkWinninghand(player, comp, playerCard, compCard) {
+    if (player > comp) {
+        results.innerText = 'Player 1 wins'
+        addDiscardPile('playerDiscard', playerCard, compCard)
+    } else if (player < comp) {
+        results.innerText = 'Computer wins'
+        addDiscardPile('compDiscard', playerCard, compCard)
+    } else {
+        results.innerText = 'Time for war'
+        war()
+    }
+
+}
+
+
+function checkWinninghandWar(player, comp, allCards) {
+    allCards = allCards.toString()
+    if (player > comp) {
+        results.innerText = 'Player 1 wins'
+        addDiscardPile('playerDiscard', allCards)
+    } else if (player < comp) {
+        results.innerText = 'Computer wins'
+        addDiscardPile('compDiscard', allCards)
+    } else {
+        results.innerText = 'Time for war'
+        war()
+    }
+
+}
+
+
+function addDiscardPileWar(pile, allCards) {
+
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/pile/${pile}/add/?cards=${allCards}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(`error ${err}`);
+        })
+}
+
+
+function addDiscardPile(pile, card1, card2) {
+
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/pile/${pile}/add/?cards=${card1},${card2}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(`error ${err}`);
+        })
+}
+
+
+function war() {
+    let allCards = []
+    let playerVal 
+    let comVal
+    const url1 = `https://deckofcardsapi.com/api/deck/${deckId}/pile/player/draw/?count=4`
+    const url2 = `https://deckofcardsapi.com/api/deck/${deckId}/pile/comp/draw/?count=4`
+    fetch(url1)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            for(let i=0; i<4; i++){
+                allCards.push(data.cards[i].code)
+            }
+            playerVal = data.cards[3].value
+        })
+        .catch(err => {
+            console.log(`error ${err}`);
+        })
+    fetch(url2)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            for (let i = 0; i < 4; i++) {
+                allCards.push(data.cards[i].code)
+            }
+            comVal = data.cards[3].value
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+    checkWinninghandWar(playerVal, comVal, allCards)
+    
 }
 
 
