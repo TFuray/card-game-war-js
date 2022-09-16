26237fbf0741e1957969cea8cec0413f16ebd950
TFuray <pk.master881@gmail.com>
2022-09-16 12:55:37 -0700

    updated logic

--- js/main2.js
+++ js/main2.js
@@ -30,13 +30,38 @@ function playCardPlayer() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
-            document.querySelector('#player1').src = data.cards[0].image
-            player1Val = convertToNum(data.cards[0].value)
-            let cardPlayed = data.cards[0].code.toString()
-            playCardComp(player1Val, cardPlayed)
+            let remaining = Number(data.piles.player.remaining)
+            if(remaining < 1){
+                returnDiscardPlayer()
+            }else{
+                document.querySelector('#player1').src = data.cards[0].image
+                player1Val = convertToNum(data.cards[0].value)
+                let cardPlayed = data.cards[0].code.toString()
+                playCardComp(player1Val, cardPlayed)
+            }
         })
         .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
+
+
+function returnDiscardPlayer(){
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/pile/playerDiscard/list/`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.piles.playerDiscard.cards)
+            let cards = []
+            let playerPile = data.piles.playerDiscard.cards
+            for(let i=0;i<playerPile.length;i++){
+                cards.push(playerPile[i].code)
+            }
+            dealPiles(cards.toString(), 'player')
+            playCardPlayer()
+            
+        })
+        .catch(err=> {
             console.log(`error ${err}`)
         })
 }
@@ -47,16 +72,41 @@ function playCardComp(player1, player1Card) {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
-            document.querySelector('#player2').src = data.cards[0].image
-            compVal = convertToNum(data.cards[0].value)
-            let cardPlayed = data.cards[0].code.toString()
-            checkWinninghand(player1, compVal, player1Card, cardPlayed)
+            let remaining = Number(data.piles.comp.remaining)
+            if (remaining < 1) {
+                returnDiscardComp()
+            }else{
+                document.querySelector('#player2').src = data.cards[0].image
+                compVal = convertToNum(data.cards[0].value)
+                let cardPlayed = data.cards[0].code.toString()
+                checkWinninghand(player1, compVal, player1Card, cardPlayed)
+            }
+           
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
 
+}
+
+
+function returnDiscardComp() {
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/pile/compDiscard/list/`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.piles.compDiscard.cards)
+            let cards = []
+            let compPile = data.piles.compDiscard.cards
+            for (let i = 0; i < compPile.length; i++) {
+                cards.push(compPile[i].code)
+            }
+            dealPiles(cards.toString(), 'comp')
+            playCardComp()
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 }
 
 
@@ -121,13 +171,13 @@ function checkWinninghandWar(player, comp, allCards) {
     allCards = allCards.toString()
     if (player > comp) {
         results.innerText = 'Player 1 wins'
-        addDiscardPile('playerDiscard', allCards)
+        addDiscardPileWar('playerDiscard', allCards)
     } else if (player < comp) {
         results.innerText = 'Computer wins'
-        addDiscardPile('compDiscard', allCards)
+        addDiscardPileWar('compDiscard', allCards)
     } else {
         results.innerText = 'Time for war'
-        war()
+        return('thats a bug ifits war again ')
     }
 
 }
@@ -152,9 +202,9 @@ function addDiscardPile(pile, card1, card2) {
     const url = `https://deckofcardsapi.com/api/deck/${deckId}/pile/${pile}/add/?cards=${card1},${card2}`
     fetch(url)
         .then(res => res.json())
-        .then(data => {
-            console.log(data)
-        })
+        // .then(data => {
+        //     console.log(data)
+        // })
         .catch(err => {
             console.log(`error ${err}`);
         })
@@ -170,7 +220,7 @@ function war() {
     fetch(url1)
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
+            console.log(data)
             for(let i=0; i<4; i++){
                 allCards.push(data.cards[i].code)
             }
@@ -182,7 +232,7 @@ function war() {
     fetch(url2)
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
+            console.log(data)
             for (let i = 0; i < 4; i++) {
                 allCards.push(data.cards[i].code)
             }
